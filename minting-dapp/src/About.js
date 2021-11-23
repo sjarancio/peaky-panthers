8c30e4a114beed96c416df0a6076e40627138116
Stephen Arancio <sjarancio@gmail.com>
2021-11-23 10:43:10 -0500

    banner change

--- minting-dapp/src/About.js
+++ minting-dapp/src/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import image1 from './banner1.jpg'
+import image1 from './webBanner1.jpg'
+import Center from './Center.css';
 
 const About = () => {
 
@@ -18,13 +19,18 @@ const About = () => {
     paddingRight: "100px",
     paddingLeft: "100px",
     paddingBottom: "100px"}
+  const bannerSize = {
+    textAlign: "center",        
+    paddingLeft: "10px",
+    }
+
   return (
     <div >
 
         <hr style={styleObj}/>
         <h1 style={titleSize} >About </h1>
-        <h6 style={bodySize}>The Peaky Panthers are a collection of 8,888 unique and hand crafted NFT's living on the Avalanche C-Chain. The Panthers team and community strives to be leaders and set the standard for NFT projects built on Avalanche. Peaky Panther's hodlers will have access to an insane amount of community driven value including things such as cash giveaways, games, competitions, and events. Get involved today and cement yourself as an OG in the Peaky Panthers community! </h6>
-        <img style={titleSize} src={image1} alt=""/>
+        <h6 style={bodySize}>The Peaky Panthers are a collection of 8,888 unique and hand crafted NFT's living on the Avalanche C-Chain. The Panthers team and community strives to be leaders and set the standard for NFT projects built on Avalanche. Peaky Panther's hodlers will have access to an insane amount of community driven value including things such as cash giveaways, games, competitions, and events. Check out our Road Map to see some of the thing we have in store! Get involved today and cement yourself as an OG in the Peaky Panthers community! Mint Date to be announced soon! </h6>
+        <img class="center-image" src={image1} alt="pp-banner"/>
     </div>
   );
 };
